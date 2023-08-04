dd3689746f3727f7eb6b99feed270d017bde5818
Azat-Bilalov <52991059+Azat-Bilalov@users.noreply.github.com>
2023-08-04 19:36:41 +0500

    add cors

--- server/index.js
+++ server/index.js
@@ -1,9 +1,13 @@
 const express = require('express');
 const app = express();
+const cors = require('cors');
 const port = 3000;
+
+app.use(cors());
 
 const todosRouter = require('./todos/app');
 app.use('/todos', todosRouter);
+
 
 app.listen(port, () => {
     console.log(`Example app listening at http://localhost:${port}`);
